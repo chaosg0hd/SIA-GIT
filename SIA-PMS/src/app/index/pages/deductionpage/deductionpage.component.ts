1ce766863dc2ec7daf9ee98cf71537c747d79c9c
santoscyrilray <78798118+santoscyrilray@users.noreply.github.com>
2021-06-22 23:51:09 +0800

    deduction Page

--- SIA-PMS/src/app/index/pages/deductionpage/deductionpage.component.ts
+++ SIA-PMS/src/app/index/pages/deductionpage/deductionpage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-deductionpage',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeductionpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SampledialogComponent, {
+      width: '500px',
+      height: '400px',
+     
+    });
+
+}
+}
+@Component({
+  selector: 'app-deductionpage',
+  templateUrl: './sampledialog.component.html',
+})
+export class SampledialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<SampledialogComponent>,
+    ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
