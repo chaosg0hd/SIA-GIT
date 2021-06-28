1656ba4d72d78e0074e7c40b2d5bd6deb0b6b656
chaosg0hd <77721525+chaosg0hd@users.noreply.github.com>
2021-06-28 14:39:48 +0800

    dddd

---
+++ MSSE-PMS/PMS/src/app/deductionpage/deductionpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deductionpage',
+  templateUrl: './deductionpage.component.html',
+  styleUrls: ['./deductionpage.component.css']
+})
+export class DeductionpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
